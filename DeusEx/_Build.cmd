c80fa8f4733907a2a8e599bf944ce321a5a3936b
USER-PC\User <Veronika-and@yandex.ru>
2019-05-15 08:23:06 +0500

    Update _Build.cmd
    Speed up rebuilding.

--- DeusEx/_Build.cmd
+++ DeusEx/_Build.cmd
@@ -1,7 +1,15 @@
+call _genBuild.cmd
+move /Y DXRVersion.uc classes\
+
 cd ..\system\
+
+ren deusexnative.dll deusexnative.dll_
+
 del deusex.u
 
 ucc make -debug -nobind
+
+ren deusexnative.dll_ deusexnative.dll
 
 copy deusex.u ..\DeusEx\
 
